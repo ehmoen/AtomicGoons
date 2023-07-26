6837a22634480a86e83366a33fcf80cf380826b0
Eirik Moen <ehmoen@gmail.com>
2023-07-26 02:09:39 +0200

    email stuff fixed

---
+++ AtomicGoons/Services/EmailSenderOptions.cs
@@ -0,0 +1,11 @@
+namespace AtomicGoons.Services;
+
+public class EmailSenderOptions
+{
+    public string SmtpServer { get; set; }
+    public int SmtpPort { get; set; }
+    public string SmtpUsername { get; set; }
+    public string SmtpPassword { get; set; }
+    public string SenderEmail { get; set; }
+    public string SenderName { get; set; }
+}
